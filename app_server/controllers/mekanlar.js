e4863358f278eef290066bdcd3d541b417f11c27
Cansu Yıldırım <88750488+cansuyildiriim@users.noreply.github.com>
2022-10-22 20:31:12 +0300

    second commit
    ödev3 upload

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,34 +1,78 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express");
 
-const anaSayfa=function(req,res){
-    res.render('anasayfa',{ "title":"Anasayfa"});
-}
-
-
-const yorumSayfasi=function(req, res) {
-    res.render('yorumekle', {"title":"Yorum Sayfası"});
+/* GET home page. */
+const anaSayfa = function (req, res, next) {
+  res.render("anasayfa", {
+    baslik: "Anasayfa",
+    sayfaBaslik: {
+      siteAd: "MekanBul",
+      slogan: "Civardaki Mekanları Keşfet!",
+    },
+    mekanlar: [
+      {
+        ad: "Starbucks",
+        puan: 4,
+        adres: "Centrum Garden AVM",
+        imkanlar: ["Dünya Kahveleri", "Kekler", "Pastalar"],
+        mesafe: "10km",
+      },
+      {
+        ad: "Gloria Jeans",
+        puan: 3,
+        adres: "Sdü Doğu Kampüsü",
+        imkanlar: ["Kahve", "Çay", "Pasta"],
+        mesafe: "5km",
+      },
+       
+    ],
+  });
 };
 
-const mekanBilgisi=function(req, res) {
-    const generateAddress = {
-        "Starbucks": " İyaş Market Girişi",
-        "Arabica" : "İyaş Avm Girişi",
-        "İyaş": "Otogar Karşısı"
-    }
-    console.log( generateAddress[req.params.title])
-    res.render('mekanbilgisi', {title:req.params.title, address: generateAddress[req.params.title]});
+const mekanBilgisi = function (req, res, next) {
+  res.render("mekanbilgisi", {
+    baslik: "Mekan Bilgisi",
+    mekanBaslik: "Starbucks",
+    mekanDetay: {
+      ad: "Starbucks",
+      puan: "4",
+      adres: "Centrum Garden AVM",
+      saatler: [
+        {
+          gunler: "Pazartesi-Cuma",
+          acilis: "9:00",
+          kapanis: "23:00",
+          kapali: false,
+        },
+        {
+          gunler: "Cumartesi-Pazar",
+          acilis: "8:00",
+          kapanis: "22:00",
+          kapali: false,
+        },
+      ],
+      imkanlar: ["Dünya Kahveleri", "Kekler", "Pastalar"],
+      koordinatlar: {
+        enlem: 37.78,
+        boylam: 30.56,
+      },
+      yorumlar: [
+        {
+          yorumYapan: "Cansu Yıldırım",
+          yorumMetni: "Kahveler güzel",
+          tarih: "20 Ekim 2022",
+          puan: "4",
+        },
+      ],
+    },
+  });
 };
 
-const yorumEkle=function(req, res, next) {
-    res.render('yorumekle', {"title":"Yorum ekle"});
+const yorumEkle = function (req, res, next) {
+  res.render("yorumekle", { title: "Yorum Ekle" });
 };
 
-module.exports={
-    anaSayfa,
-    mekanBilgisi,
-    yorumEkle,
-    yorumSayfasi
-    
-}
-
+module.exports = {
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+};
