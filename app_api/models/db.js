45fc72b5f318bac006f56144668f684cbb4d0fbd
Cansu Yıldırım <88750488+cansuyildiriim@users.noreply.github.com>
2022-12-04 20:34:55 +0300

    odev7

---
+++ app_api/models/db.js
@@ -0,0 +1,32 @@
+var mongoose = require("mongoose");
+
+// var dbURI = "mongodb://localhost/mekanbul";
+var dbURI = "mongodb+srv://cansuyildirim:1234@mekanbul.rhpaw4c.mongodb.net/mekanbul?retryWrites=true&w=majority"
+mongoose.connect(dbURI);
+
+function kapat(msg, callback) {
+  mongoose.connection.close(() => {
+    console.log(msg);
+    callback();
+  });
+}
+
+process.on("SIGINT", () => {
+  kapat("Uygulama kapatıldı", () => {
+    process.exit(0);
+  });
+});
+
+mongoose.connection.on("connected", () => {
+  console.log(dbURI + " adresindeki veritabanına bağlandı");
+});
+
+mongoose.connection.on("disconnect", () => {
+  console.log(dbURI + " adresindeki veritabanı bağlantısı koptu");
+});
+
+mongoose.connection.on("error", () => {
+  console.log("Bağlantı hatası");
+});
+
+require("./mekansema");
