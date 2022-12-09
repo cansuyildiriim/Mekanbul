810f5a78a43cb00d1d782b01dc1bb576510b7761
Cansu Yıldırım <88750488+cansuyildiriim@users.noreply.github.com>
2022-12-09 13:55:30 +0300

    odev8 upload

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 var createError = require('http-errors');
 var express = require('express');
-var session =require('express-session')
+var session=require('express-session')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-require('./app_api/models/db'); //veritabanını uygulamaya tanıttık
+require('./app_api/models/db'); 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
+
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'pug');
 app.use(session({
   secret:'gizli',
   cookie:{maxAge:1000*60*60*24},
   resave:true,
   saveUninitialized:true
 }));
-// view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -32,18 +32,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  
   res.status(err.status || 500);
   res.render('error');
 });
